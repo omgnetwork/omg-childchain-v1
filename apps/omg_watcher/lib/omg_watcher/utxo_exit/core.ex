f473f59c707a549ce0d4ae1f998efe41cc38f72d
Arthur Chiu <hallo@achiurizo.co>
2020-01-15 21:44:55 +0000

    compose_deposit_standard_exit/2 to use ex_plasma

--- apps/omg_watcher/lib/omg_watcher/utxo_exit/core.ex
+++ apps/omg_watcher/lib/omg_watcher/utxo_exit/core.ex
@@ -44,18 +44,31 @@ defmodule OMG.Watcher.UtxoExit.Core do
           {:error, :no_deposit_for_given_blknum}
           | {:ok, %{utxo_pos: non_neg_integer, txbytes: binary, proof: binary}}
   def compose_deposit_standard_exit({:ok, {db_utxo_pos, db_utxo_value}}) do
+    # TODO(achiurizo)
+    # Create private functions here to pattern match the value into ex_plasma structs
+    # so we can remove the Utxo.Position and output
     utxo_pos = OMG.Utxo.Position.from_db_key(db_utxo_pos)
-
     %Utxo{output: %OMG.Output{amount: amount, currency: currency, owner: owner}} = Utxo.from_db_value(db_utxo_value)
 
-    tx = Transaction.Payment.new([], [{owner, currency, amount}])
-    txs = [Transaction.Signed.encode(%Transaction.Signed{raw_tx: tx, sigs: []})]
+    utxo = %ExPlasma.Utxo{owner: owner, currency: currency, amount: amount}
+    tx = ExPlasma.Transactions.Deposit.new(%{sigs: [], inputs: [], outputs: [utxo]})
+    tx_rlp = ExPlasma.Transaction.to_rlp(tx)
+    raw_txbytes = ExPlasma.Transaction.encode(tx)
+
+    # TODO(achiurizo)
+    # hacks, this proof inclusion needs to include
+    # the empty signatures otherwise it will return an error
+    # about malformed_witnesses.
+    #
+    # See: https://github.com/omisego/elixir-omg/issues/1263
+    unsigned_txbytes = ExRLP.encode([ [] | tx_rlp])
+    proof = unsigned_txbytes |> List.wrap() |> Block.inclusion_proof(0)
 
     {:ok,
      %{
        utxo_pos: Utxo.Position.encode(utxo_pos),
-       txbytes: Transaction.raw_txbytes(tx),
-       proof: Block.inclusion_proof(txs, 0)
+       txbytes: raw_txbytes,
+       proof: proof
      }}
   end
 
