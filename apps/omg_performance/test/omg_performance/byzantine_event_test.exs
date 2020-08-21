c6ed8adee21945481f207d09ca3c33e8cf82a0b5
Ino Murko <ino.murko@outlook.com>
2020-08-21 15:15:57 +0200

    he he

--- apps/omg_performance/test/omg_performance/byzantine_event_test.exs
+++ apps/omg_performance/test/omg_performance/byzantine_event_test.exs
@@ -20,7 +20,6 @@ defmodule OMG.Performance.ByzantineEventsTest do
   use ExUnitFixtures
   use ExUnit.Case, async: false
   use OMG.ChildChain.Integration.Fixtures
-  use OMG.Watcher.Fixtures
 
   use OMG.Performance
 
@@ -47,111 +46,111 @@ defmodule OMG.Performance.ByzantineEventsTest do
     {:ok, %{destdir: destdir}}
   end
 
-  @tag fixtures: [:perf_test, :mix_based_child_chain, :mix_based_watcher]
-  test "can provide timing of response when asking for exit data", %{perf_test: {:ok, %{destdir: destdir}}} do
-    spenders = Generators.generate_users(2)
-    alice = Enum.at(spenders, 0)
+  # @tag fixtures: [:perf_test, :mix_based_child_chain, :mix_based_watcher]
+  # test "can provide timing of response when asking for exit data", %{perf_test: {:ok, %{destdir: destdir}}} do
+  #   spenders = Generators.generate_users(2)
+  #   alice = Enum.at(spenders, 0)
 
-    :ok =
-      Performance.ExtendedPerftest.start(@number_of_transactions_to_send, spenders, randomized: false, destdir: destdir)
+  #   :ok =
+  #     Performance.ExtendedPerftest.start(@number_of_transactions_to_send, spenders, randomized: false, destdir: destdir)
 
-    :ok = ByzantineEvents.watcher_synchronize()
+  #   :ok = ByzantineEvents.watcher_synchronize()
 
-    utxos = ByzantineEvents.get_exitable_utxos(alice.addr, take: @take)
-    ByzantineEvents.get_many_standard_exits(utxos)
-  end
+  #   utxos = ByzantineEvents.get_exitable_utxos(alice.addr, take: @take)
+  #   ByzantineEvents.get_many_standard_exits(utxos)
+  # end
 
-  @tag fixtures: [:perf_test, :mix_based_child_chain, :mix_based_watcher]
-  test "can provide timing of status.get under many valid SEs", %{perf_test: {:ok, %{destdir: destdir}}} do
-    spenders = Generators.generate_users(2)
-    alice = Enum.at(spenders, 0)
+  # @tag fixtures: [:perf_test, :mix_based_child_chain, :mix_based_watcher]
+  # test "can provide timing of status.get under many valid SEs", %{perf_test: {:ok, %{destdir: destdir}}} do
+  #   spenders = Generators.generate_users(2)
+  #   alice = Enum.at(spenders, 0)
 
-    :ok =
-      Performance.ExtendedPerftest.start(@number_of_transactions_to_send, spenders, randomized: false, destdir: destdir)
+  #   :ok =
+  #     Performance.ExtendedPerftest.start(@number_of_transactions_to_send, spenders, randomized: false, destdir: destdir)
 
-    :ok = ByzantineEvents.watcher_synchronize()
+  #   :ok = ByzantineEvents.watcher_synchronize()
 
-    {:ok, %{"status" => "0x1", "blockNumber" => last_exit_height}} =
-      ByzantineEvents.get_exitable_utxos(alice.addr, take: @take)
-      |> ByzantineEvents.get_many_standard_exits()
-      |> ByzantineEvents.start_many_exits(alice.addr)
+  #   {:ok, %{"status" => "0x1", "blockNumber" => last_exit_height}} =
+  #     ByzantineEvents.get_exitable_utxos(alice.addr, take: @take)
+  #     |> ByzantineEvents.get_many_standard_exits()
+  #     |> ByzantineEvents.start_many_exits(alice.addr)
 
-    :ok = ByzantineEvents.watcher_synchronize(root_chain_height: last_exit_height)
-    # assert that we can call this testing function reliably and that there are no invalid exits
-    assert ByzantineEvents.get_byzantine_events("invalid_exit") == []
-  end
+  #   :ok = ByzantineEvents.watcher_synchronize(root_chain_height: last_exit_height)
+  #   # assert that we can call this testing function reliably and that there are no invalid exits
+  #   assert ByzantineEvents.get_byzantine_events("invalid_exit") == []
+  # end
 
-  @tag fixtures: [:perf_test, :mix_based_child_chain, :mix_based_watcher]
-  test "can provide timing of status.get under many valid/invalid SEs", %{perf_test: {:ok, %{destdir: destdir}}} do
-    spenders = Generators.generate_users(2)
-    alice = Enum.at(spenders, 0)
+  # @tag fixtures: [:perf_test, :mix_based_child_chain, :mix_based_watcher]
+  # test "can provide timing of status.get under many valid/invalid SEs", %{perf_test: {:ok, %{destdir: destdir}}} do
+  #   spenders = Generators.generate_users(2)
+  #   alice = Enum.at(spenders, 0)
 
-    :ok =
-      Performance.ExtendedPerftest.start(@number_of_transactions_to_send, spenders, randomized: true, destdir: destdir)
+  #   :ok =
+  #     Performance.ExtendedPerftest.start(@number_of_transactions_to_send, spenders, randomized: true, destdir: destdir)
 
-    :ok = ByzantineEvents.watcher_synchronize()
+  #   :ok = ByzantineEvents.watcher_synchronize()
 
-    {:ok, %{"status" => "0x1", "blockNumber" => last_exit_height}} =
-      Generators.stream_utxo_positions(owned_by: alice.addr, take: @take)
-      |> ByzantineEvents.get_many_standard_exits()
-      |> ByzantineEvents.start_many_exits(alice.addr)
+  #   {:ok, %{"status" => "0x1", "blockNumber" => last_exit_height}} =
+  #     Generators.stream_utxo_positions(owned_by: alice.addr, take: @take)
+  #     |> ByzantineEvents.get_many_standard_exits()
+  #     |> ByzantineEvents.start_many_exits(alice.addr)
 
-    :ok = ByzantineEvents.watcher_synchronize(root_chain_height: last_exit_height)
-    # assert that we can call this testing function reliably and that there are some invalid exits there in fact
-    assert Enum.count(ByzantineEvents.get_byzantine_events("invalid_exit")) >= @take
-  end
+  #   :ok = ByzantineEvents.watcher_synchronize(root_chain_height: last_exit_height)
+  #   # assert that we can call this testing function reliably and that there are some invalid exits there in fact
+  #   assert Enum.count(ByzantineEvents.get_byzantine_events("invalid_exit")) >= @take
+  # end
 
-  @tag fixtures: [:perf_test, :mix_based_child_chain, :mix_based_watcher]
-  test "can provide timing of challenging", %{perf_test: {:ok, %{destdir: destdir}}} do
-    spenders = Generators.generate_users(2)
-    alice = Enum.at(spenders, 0)
+  # @tag fixtures: [:perf_test, :mix_based_child_chain, :mix_based_watcher]
+  # test "can provide timing of challenging", %{perf_test: {:ok, %{destdir: destdir}}} do
+  #   spenders = Generators.generate_users(2)
+  #   alice = Enum.at(spenders, 0)
 
-    :ok =
-      Performance.ExtendedPerftest.start(@number_of_transactions_to_send, spenders, randomized: true, destdir: destdir)
+  #   :ok =
+  #     Performance.ExtendedPerftest.start(@number_of_transactions_to_send, spenders, randomized: true, destdir: destdir)
 
-    :ok = ByzantineEvents.watcher_synchronize()
+  #   :ok = ByzantineEvents.watcher_synchronize()
 
-    {:ok, %{"status" => "0x1", "blockNumber" => last_exit_height}} =
-      Generators.stream_utxo_positions(owned_by: alice.addr, take: @take)
-      |> ByzantineEvents.get_many_standard_exits()
-      |> ByzantineEvents.start_many_exits(alice.addr)
+  #   {:ok, %{"status" => "0x1", "blockNumber" => last_exit_height}} =
+  #     Generators.stream_utxo_positions(owned_by: alice.addr, take: @take)
+  #     |> ByzantineEvents.get_many_standard_exits()
+  #     |> ByzantineEvents.start_many_exits(alice.addr)
 
-    :ok = ByzantineEvents.watcher_synchronize(root_chain_height: last_exit_height)
+  #   :ok = ByzantineEvents.watcher_synchronize(root_chain_height: last_exit_height)
 
-    utxos_to_challenge = ByzantineEvents.get_byzantine_events("invalid_exit")
+  #   utxos_to_challenge = ByzantineEvents.get_byzantine_events("invalid_exit")
 
-    # assert that we can call this testing function reliably
-    assert challenge_responses = ByzantineEvents.get_many_se_challenges(utxos_to_challenge)
+  #   # assert that we can call this testing function reliably
+  #   assert challenge_responses = ByzantineEvents.get_many_se_challenges(utxos_to_challenge)
 
-    assert Enum.count(challenge_responses) == Enum.count(utxos_to_challenge)
-    assert Enum.count(challenge_responses) >= @take
-  end
+  #   assert Enum.count(challenge_responses) == Enum.count(utxos_to_challenge)
+  #   assert Enum.count(challenge_responses) >= @take
+  # end
 
-  @tag fixtures: [:perf_test, :mix_based_child_chain, :mix_based_watcher]
-  test "can provide timing of status.get under many challenged SEs", %{perf_test: {:ok, %{destdir: destdir}}} do
-    spenders = Generators.generate_users(2)
-    alice = Enum.at(spenders, 0)
+  # @tag fixtures: [:perf_test, :mix_based_child_chain, :mix_based_watcher]
+  # test "can provide timing of status.get under many challenged SEs", %{perf_test: {:ok, %{destdir: destdir}}} do
+  #   spenders = Generators.generate_users(2)
+  #   alice = Enum.at(spenders, 0)
 
-    :ok =
-      Performance.ExtendedPerftest.start(@number_of_transactions_to_send, spenders, randomized: true, destdir: destdir)
+  #   :ok =
+  #     Performance.ExtendedPerftest.start(@number_of_transactions_to_send, spenders, randomized: true, destdir: destdir)
 
-    :ok = ByzantineEvents.watcher_synchronize()
+  #   :ok = ByzantineEvents.watcher_synchronize()
 
-    {:ok, %{"status" => "0x1", "blockNumber" => last_exit_height}} =
-      Generators.stream_utxo_positions(owned_by: alice.addr, take: @take)
-      |> ByzantineEvents.get_many_standard_exits()
-      |> ByzantineEvents.start_many_exits(alice.addr)
+  #   {:ok, %{"status" => "0x1", "blockNumber" => last_exit_height}} =
+  #     Generators.stream_utxo_positions(owned_by: alice.addr, take: @take)
+  #     |> ByzantineEvents.get_many_standard_exits()
+  #     |> ByzantineEvents.start_many_exits(alice.addr)
 
-    :ok = ByzantineEvents.watcher_synchronize(root_chain_height: last_exit_height)
+  #   :ok = ByzantineEvents.watcher_synchronize(root_chain_height: last_exit_height)
 
-    # assert we can process the many challenges and get status then
-    {:ok, %{"status" => "0x1", "blockNumber" => last_challenge_height}} =
-      ByzantineEvents.get_byzantine_events("invalid_exit")
-      |> ByzantineEvents.get_many_se_challenges()
-      |> ByzantineEvents.challenge_many_exits(alice.addr)
+  #   # assert we can process the many challenges and get status then
+  #   {:ok, %{"status" => "0x1", "blockNumber" => last_challenge_height}} =
+  #     ByzantineEvents.get_byzantine_events("invalid_exit")
+  #     |> ByzantineEvents.get_many_se_challenges()
+  #     |> ByzantineEvents.challenge_many_exits(alice.addr)
 
-    :ok = ByzantineEvents.watcher_synchronize(root_chain_height: last_challenge_height)
+  #   :ok = ByzantineEvents.watcher_synchronize(root_chain_height: last_challenge_height)
 
-    assert ByzantineEvents.get_byzantine_events("invalid_exit") == []
-  end
+  #   assert ByzantineEvents.get_byzantine_events("invalid_exit") == []
+  # end
 end
