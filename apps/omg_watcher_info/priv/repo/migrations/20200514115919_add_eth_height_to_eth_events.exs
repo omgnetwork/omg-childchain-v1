7428b297464e0aefad8a53381c04b2eb01567e75
ino.murko@outlook.com <ino.murko@outlook.com>
2020-08-28 12:08:41 +0200

    remove watcher

--- apps/omg_watcher_info/priv/repo/migrations/20200514115919_add_eth_height_to_eth_events.exs
+++
@@ -1,9 +0,0 @@
-defmodule OMG.WatcherInfo.DB.Repo.Migrations.AddEthHeightToEthEvents do
-  use Ecto.Migration
-
-  def up() do
-    alter table(:ethevents) do
-      add(:eth_height, :integer)
-    end
-  end
-end
