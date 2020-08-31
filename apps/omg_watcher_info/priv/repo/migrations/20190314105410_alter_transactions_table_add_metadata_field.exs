d495998d2226d36944983b264bba32f4e5172e9c
Ino Murko <ino.murko@outlook.com>
2020-09-01 01:14:21 +0200

    inomurko strip watcher from childchain (#3)
    * remove watchers from childchain

--- apps/omg_watcher_info/priv/repo/migrations/20190314105410_alter_transactions_table_add_metadata_field.exs
+++
@@ -1,9 +0,0 @@
-defmodule OMG.WatcherInfo.DB.Repo.Migrations.AlterTransactionsTableAddMetadataField do
-  use Ecto.Migration
-
-  def change() do
-    alter table(:transactions) do
-      add(:metadata, :binary)
-    end
-  end
-end
