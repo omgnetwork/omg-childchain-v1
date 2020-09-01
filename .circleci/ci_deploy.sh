8b9d96f2f26fe96aaf35790d0f73e92a3165cdb2
Ino Murko <ino.murko@outlook.com>
2020-09-02 00:07:54 +0200

    building childchain is enough (#4)
    * building childchain is enough

--- .circleci/ci_deploy.sh
+++ .circleci/ci_deploy.sh
@@ -16,9 +16,7 @@ echo_warn() {
 
 if [ -z "$CIRCLE_GPG_KEY" ] ||
        [ -z "$CIRCLE_GPG_OWNERTRUST" ] ||
-       [ -z "$GCP_KEY_FILE" ] ||
-       [ -z "$GCP_ACCOUNT_ID" ] ||
-       [ -z "$GCP_ZONE" ]; then
+       [ -z "$GCLOUD_SERVICE_KEY" ]; then
     echo_warn "Deploy credentials not present, skipping deploy."
     exit 0
 fi
@@ -37,21 +35,11 @@ printf "%s\\n" "$CIRCLE_GPG_OWNERTRUST" | gpg --import-ownertrust
 ## GCP
 ##
 
-GCPFILE=$(mktemp)
-trap 'rm -f $GCPFILE' 0 1 2 3 6 14 15
-echo "$GCP_KEY_FILE" | base64 -d > "$GCPFILE"
-
-gcloud auth activate-service-account --key-file="$GCPFILE"
+echo $GCLOUD_SERVICE_KEY | gcloud auth activate-service-account --key-file=-
 gcloud beta container clusters get-credentials ${GCP_CLUSTER_DEVELOPMENT} --region ${GCP_REGION} --project ${GCP_PROJECT}
 image_tag="$(printf "%s" "$CIRCLE_SHA1" | head -c 7)"
 
-if [ "$DEPLOY" = "watcher" ]; then
-    kubectl set image statefulset watcher watcher=omisego/watcher:${image_tag}
-    while true; do if [ "$(kubectl get pods watcher-0 -o jsonpath=\"{.status.phase}\" | grep Running)" ]; then break; fi; done
-elif [ "$DEPLOY" = "watcher_info" ]; then
-    kubectl set image statefulset watcher-info watcher-info=omisego/watcher_info:${image_tag}
-    while true; do if [ "$(kubectl get pods watcher-info-0 -o jsonpath=\"{.status.phase}\" | grep Running)" ]; then break; fi; done
-else
-    kubectl set image statefulset childchain childchain=omisego/child_chain:${image_tag}
-    while true; do if [ "$(kubectl get pods childchain-0 -o jsonpath=\"{.status.phase}\" | grep Running)" ]; then break; fi; done
-fi;
+kubectl set image statefulset childchain childchain=omisego/child_chain:${image_tag}
+while true; do if [ "$(kubectl get pods childchain-0 -o jsonpath=\"{.status.phase}\" | grep Running)" ]; then break; fi; done
+
+
